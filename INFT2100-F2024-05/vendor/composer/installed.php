2681f3a6aa7a661f2cbe7a309dbc688c78511f28
Mateo Valles <114031093+NPC-239@users.noreply.github.com>
2024-09-30 20:43:57 -0400

    in class laravel example

---
+++ INFT2100-F2024-05/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2d66d5e614122191c6ea691a3bf48ac3c0667b56',
+        'type' => 'object',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2d66d5e614122191c6ea691a3bf48ac3c0667b56',
+            'type' => 'object',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
