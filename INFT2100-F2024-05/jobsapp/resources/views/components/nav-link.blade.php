684baadecd5d1e8147c3db4c540e7d109b61a8a3
Mateo Valles <114031093+NPC-239@users.noreply.github.com>
2024-10-11 17:23:09 -0400

    in class 3.2 update part 1

---
+++ INFT2100-F2024-05/jobsapp/resources/views/components/nav-link.blade.php
@@ -0,0 +1,20 @@
+<!-- the attribute inside the array is utilizing the key 'active' -->
+@props(['active' => false, 'type'])
+
+@if( $type == 'a')
+    <a class="{{ $active ? 'bg-gray-900 text-white'
+                         : 'text-gray-300 hover:bg-gray-700 hover:text-white'}}
+                            rounded-md px-3 py-2 text-sm font-medium"
+                            aria-current="{{ $active ? 'page' : 'false' }}"
+    {{ $attributes }}>
+    {{ $slot }}
+    </a>
+@else
+    <button class="{{ $active ? 'bg-gray-900 text-white'
+                         : 'text-gray-300 hover:bg-gray-700 hover:text-white'}}
+                            rounded-md px-3 py-2 text-sm font-medium"
+       aria-current="{{ $active ? 'page' : 'false' }}"
+        {{ $attributes }}>
+        {{ $slot }}
+    </button>
+@endif
